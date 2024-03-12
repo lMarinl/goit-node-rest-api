f8d6f36a8fbb58e1a3e035f4380bc101fe30f9a5
lMarinl <marutunan67@gmail.com>
2024-03-12 15:18:55 +0200

    add contactsRouter.patch, hooks and models

---
+++ middlewares/isValidId.js
@@ -0,0 +1,11 @@
+import { isValidObjectId } from "mongoose"
+import HttpError from "../helpers/HttpError.js"
+const isValidId = (req, _, next) => {
+  const { id } = req.params
+  if (!isValidObjectId(id)) {
+    return next(HttpError(404, `${id} is not a valid id`))
+  }
+  next()
+}
+
+export default isValidId
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,61 +1,30 @@
 import fs from "fs/promises"
 import path from "path"
 import { nanoid } from "nanoid"
+import Contact from "../models/contact.js"
 
 const contactsPath = path.resolve("db", "contacts.json")
 
- async function listContacts() {
-  const data = await fs.readFile(contactsPath)
-  return JSON.parse(data)
-}
+const listContacts = () => Contact.find()
 
- async function getContactById(contactId) {
-  const contacts = await listContacts()
-  const contact = contacts.find((contact) => contact.id === contactId)
+const getContactById = (_id) => Contact.findById(_id)
 
-  return contact || null
-}
+const removeContact = (_id) => Contact.findByIdAndDelete(_id)
 
- async function removeContact(contactId) {
-  const contacts = await listContacts()
-  const index = contacts.findIndex((contact) => contact.id === contactId)
-  if (index === -1) {
-    return null
-  }
-  const [deletedContact] = contacts.splice(index, 1)
-  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+const addContact = (data) => Contact.create(data)
 
-  return deletedContact
-}
+const updateContactById = (_id, data) => Contact.findByIdAndUpdate(_id, data)
 
- async function addContact({ name, email, phone }) {
-  const contacts = await listContacts()
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  }
-  contacts.push(newContact)
-  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
-  return newContact
-}
-
- async function updateContactById(id, data) {
-  const contacts = await listContacts()
-  const index = contacts.findIndex((contact) => contact.id === id)
-  if (index === -1) {
-    return null
-  }
-  contacts[index] = { ...contacts[index], ...data }
-  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
-  return contacts[index]
-}
+const updateFavorite = (_id, data) =>
+  Contact.findByIdAndUpdate(_id, data, {
+    new: true,
+  })
 
 export default {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContactById
+  updateContactById,
+  updateFavorite,
 }
